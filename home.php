3dd1ce6ff93c756bd359bbc135c9eed4c2a8fb47
Prithvi Raj singh Bisht <prithvisbisht@gmail.com>
2018-03-29 14:52:06 +0530

    Lister file removed and a ne file is added

--- home.php
+++ home.php
@@ -59,13 +59,86 @@ else{
     <link href="css/style.css" rel="stylesheet">
   </head>
   <body>
-    <?php
-    $temp=getcwd();
-    $temp=$temp.'/upload/'.$username.'/';
-    echo $temp;
-    chdir($temp);
-    echo getcwd();
-    include("temp.php"); ?>
+    <div class="row">
+      <div class="left col-md-2" style="height:700px">
+        <div class="">
+          <img  class="logo"  src="img/cloud.svg" alt="logo"><br>
+          <ul >
+            <li><h5><a class="menu" href="#">Home</a></h5></li>
+            <li><h5><a  class="menu" href="">Files</a></h5></li>
+            <li><h5><a class="menu" href="">My Account</a></h5></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-10" style="height:600px">
+        <div class="row top" style="height:100px">
+          <h1 class="main-text">Welcome <?php echo $_SESSION['username']?></h1>
+          <form class="form-inline search">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+        <div class="row" style="height:600px">
+          <div class="middle col-md-9" style="height:600px">
+            <h1>HEllo World</h1>
+            <?php
+          //   $name=$_SESSION['username'];
+          //   //echo $name;
+          //   $dir = "./upload/".$name."";
+          //   $target="./upload";
+          //
+          // // Open a directory, and read its contents
+          // if (is_dir($dir)){
+          //   if ($dh = opendir($dir)){
+          //     while (($file = readdir($dh)) !== false){
+          //       //echo "filename:" . $file . "<br>";
+          //       if($file!=".." && $file!=".")
+          //       echo "<a href=".$target."/".$name."/".$file.">.".$file.".</a><br>";
+          //     }
+          //     closedir($dh);
+          //   }
+          // }
+          //   echo "<a href=./".$target."/".$name.">click to see files</a>";
+          //   //session_destroy();
+          $dir = new RecursiveDirectoryIterator('./upload/'.$username,FilesystemIterator::SKIP_DOTS);
+          $it = new RecursiveIteratorIterator($dir,RecursiveIteratorIterator::SELF_FIRST);
+          $it->setMaxDepth(2);
+          foreach ($it as $fileinfo) {
+            if ($fileinfo->isDir()) {
+              printf("Folder - %s\n", $fileinfo->getFilename());
+            } elseif ($fileinfo->isFile()) {
+              printf("File From %s - %s\n", $it->getSubPath(), $fileinfo->getFilename());
+            }
+          }
+          ?>
+          </div>
+          <div class="right col-md-3"style="height:600px">
+            <button class="btn btn-outline-warning" type="button" data-toggle="modal" data-target="#folder">upload folder</button><br>
+            <button class="btn btn-outline-warning" type="button" data-toggle="modal" data-target="#file">upload files</button>
+            <div class="modal fade" id="folder" tabindex="-1" role="dialog" aria-labelledby="Title" aria-hidden="true">
+              <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="Title">Choose folder to upload</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    <form class="form-group" method="POST" enctype="multipart/form-data" >
+                      <input type="text" name="folder_name" placeholder="Enter Folder Name" required>
+                      <input class="form-control-file" type="file" name="files[]" id="files" multiple="" directory="" webkitdirectory="" mozdirectory="">description <input type="textarea" name="description" rows="8" cols="80">
+                      <input type="submit" name="upload_folder" class="btn btn-secondary">
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
     <!-- Bootstrap tooltips -->
     <script type="text/javascript" src="js/popper.min.js"></script>
---
+++ temp.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+$username=$_SESSION['username'];
+$temp=getcwd();
+$temp=$temp.'/upload/'.$username.'/';
+chdir($temp);
+$path=$temp;
+//echo getcwd();
+
+// include './index.php';
+function ListFolder($path)
+{
+    //using the opendir function
+    $dir_handle = @opendir($path) or die("Unable to open $path");
+
+    //Leave only the lastest folder name
+    $list=explode("/",$path);
+    $dirname = end($list);
+
+    //display the target folder.
+    echo ("<li>$dirname\n");
+    echo "<ul>\n";
+    while (false !== ($file = readdir($dir_handle)))
+    {
+        if($file!="." && $file!="..")
+        {
+            if (is_dir($path."/".$file))
+            {
+                //Display a list of sub folders.
+                ListFolder($path."/".$file);
+            }
+            else
+            {
+                //Display a list of files.
+                echo "<li>$file</li>";
+            }
+        }
+    }
+    echo "</ul>\n";
+    echo "</li>\n";
+
+    //closing the directory
+    closedir($dir_handle);
+}
+
+ListFolder($path);
+?>
